6e82d313bc4dec21f6bdbab7907b4b55356a8816
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-04-06 16:31:44 +0100

    Formatted response by filtering better.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
    const data = await fetchdata()
-    console.log(data)
+    //console.log(data)
 
     res.render("index", { 
        ...data
